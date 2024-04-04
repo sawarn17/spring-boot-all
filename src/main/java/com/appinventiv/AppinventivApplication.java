40917efb39c8d1e4b7a6c0bc8dd74bc4b9ce36fb
sawarn17 <ayamshifo@gmail.com>
2024-04-04 11:13:16 +0530

    baisc setup of AOP done

--- src/main/java/com/appinventiv/AppinventivApplication.java
+++ src/main/java/com/appinventiv/AppinventivApplication.java
@@ -2,8 +2,10 @@ package com.appinventiv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy //this is used for to config the AOP
 public class AppinventivApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/appinventiv/aopservices/LoggingAspect.java
@@ -0,0 +1,39 @@
+package com.appinventiv.aopservices;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Aspect // it contains advice methods
+@Component
+public class LoggingAspect {
+	
+	@Autowired ObjectMapper mapper;
+
+	Logger log = LoggerFactory.getLogger(LoggingAspect.class);
+
+	// here we provide the package in terms 
+	@Pointcut(value = "execution(* com.appinventiv.controller.UserController.findAllUsers(..) )")
+	public void myPointcut() {
+
+	}
+
+	@Around("myPointcut()")
+	public Object applicationLogger(ProceedingJoinPoint pjp) throws Throwable {
+		String methodName = pjp.getSignature().getName();
+		String className = pjp.getTarget().getClass().toString();
+		Object[] array = pjp.getArgs();
+		log.info("method invoked " + className + " : " + methodName + "()" + "arguments : "
+				+ mapper.writeValueAsString(array));
+		Object object = pjp.proceed();
+		log.info(className + " : " + methodName + "()" + "Response : " + mapper.writeValueAsString(object));
+		return object;
+	}
+}
